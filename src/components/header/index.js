96be154c4e429e0ebe0a0c1ea8889133da4bc73a
ayomideRussianCode <ayindeayomide7@gmail.com>
2024-10-09 12:14:16 +0100

    new fixes

---
+++ src/components/header/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Background, ButtonLink,  Container, Logo } from "./styles/header";
+import{ Link as ReactRouterLink } from 'react-router-dom';
+
+
+export default function Header({ bg = true, children, ...restProps }) {
+  return bg ? <Background {...restProps}>{children}</Background> : children;
+}
+
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Header.ButtonLink = function ButtonLink({children, ...restProps}) {
+  return <Container{...restProps}>{children}</Container>
+}
+
+Header.Logo = function HeaderLogo({ to, ...restProps }) {
+  return (
+    <ReactRouterLink to={to}>
+      <Logo {...restProps} />
+    </ReactRouterLink>
+  );
+};
---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { JumbotronContainer } from "../containers/jumbotron";
+import { FaqsContainer } from "../containers/faqs";
+
+export default function Home() {
+  return (
+    <>
+    <HeaderContainer>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </HeaderContainer>
+    </>
+  );
+}
